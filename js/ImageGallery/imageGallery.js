da92d478fdb4eeba2645c9282ce42d6dde14ca7e
Jeremias Ceciliano E <jecel0911@gmail.com>
2014-03-25 23:16:19 -0600

    New Form for New Items, Add and serch partiall
    new form for adding new Items. Functions for filtering and saving new
    elements

--- js/ImageGallery/imageGallery.js
+++ js/ImageGallery/imageGallery.js
@@ -1,4 +1,21 @@
 
+/*
+$(document).ready(function(){
+
+	$.ajax
+    ({
+        type: "POST",
+        url: 'http://localhost:3000/ImageGallery',
+        data: 'json=' + JSON.stringify({ "Description": "Probando insercion desde JQuery"}),
+        success: function () {
+
+        alert("Thanks!"); 
+        }
+    });
+
+});
+*/
+
 $(document).ready(function(){
 	//action Filter on button.
 	$('#btnFilter').on('click',function(){
@@ -8,16 +25,64 @@ $(document).ready(function(){
 	$('#inputFilter').keyup(function(){
 		renderListitems($('#inputFilter').val());
 	});
+	//action show Panel for adding new Item
+	$('#btnShowAddNewItem').on('click',function(e){
+		e.preventDefault();
+		$('#frmAddItem').show('toggle');
+		$('#frmFind').hide();
+		$('#txtItem').val('').focus();
+	});
+	//action hide panel for adding new Item
+	$('#btnHideAddNewItem').on('click',function(e){
+		e.preventDefault();
+		$('#frmFind').show('toggle');
+		$('#frmAddItem').hide();
+		$('#inputText').val('').focus();
+	});
+	//action for save new item
+	$('#btnAddItem').on('click',function(){
+		var text = $('#txtItem').val();
+		if(text == ''){
+			alert('Description is required');
+			$('#txtItem').focus();
+		}else{
+			saveNewItem(text);
+		}
+	});
 });
 
 
 //render the list filtered
 function renderListitems(queryString){
-	var result = findListItem(queryString);
-	for(i=0;i<result.list.length;i++){
-		var r = _.template(window.templates.T_itemList,result.list[i]);
-		$('.panelIzquierdo .contenido ul').append(r);
-	}
+	$.ajax({
+        type: "GET",
+        url: 'http://localhost:3000/ImageGallery',
+        data: 'json=' + JSON.stringify({ "queryString": queryString}),
+        success: function (result,msg) {
+        	$('.panelIzquierdo .contenido ul').html('');
+        	for(i=0;i<result.length;i++){
+				//var r = _.template(window.templates.T_itemList,JSON.stringify(result[i]));
+				var r = "<li id='item_"+result[i]._id+"'>"+result[i].Description+"</li>";
+				$('.panelIzquierdo .contenido ul').append(r);
+				$('#item_'+result[i]._id).show('toggle');
+			}
+        }
+    });
+}
+
+//save the new Item in BD
+function saveNewItem(description){
+	$.ajax({
+	        type: "POST",
+	        url: 'http://localhost:3000/ImageGallery',
+	        data: 'json=' + JSON.stringify({ "Description": description}),
+	        success: function (result,msg) {
+				//var r = _.template(window.templates.T_itemList,JSON.stringify(result[i]));
+				var r = "<li id='"+result._id+"'>"+result.Description+"</li>";
+				$('.panelIzquierdo .contenido ul').append(r);
+	        	$('#btnHideAddNewItem').click();
+	        }
+	    });
 }
 
 //render the detail of the item
@@ -27,18 +92,3 @@ function renderDetailedItem(){
 
 
 /****************************************************/
-
-//find the list of items in db
-function findListItem(queryString){
-	// in proces... read from mongo the list of items.
-	var list = {
-		"detail": "detailed",
-		"list":[
-			{"id":1,"text":'Question 1'} /*,
-			{"id":2,"text":'Question 2'},
-			{"id":3,"text":'Question 3'},
-			{"id":4,"text":'Question 4'} */
-		]
-	}
-	return list;
-}
--- js/ImageGallery/imageGalleryTemplates.js
+++ js/ImageGallery/imageGalleryTemplates.js
@@ -1,3 +1,3 @@
 
 window.templates = {};
-window.templates.T_itemList = '<li id="<%= id %>"> <%= text %> </li>';
+window.templates.T_itemList = '<li id="<%= _id %>"> <%= Description %> </li>';
